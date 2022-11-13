528196086d21697a41d7a3365925ccac5e70346b
Carlos-abc <72826660+Carlos-abc@users.noreply.github.com>
2022-11-12 20:57:17 -0800

    Add files via upload

---
+++ Actividad de taller 4.1.2: Exploración de Maven/App.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import java.sql.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Connection connection = null;
+        try {
+            // below two lines are used for connectivity.
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/mydb",
+                "mydbuser", "mydbuser");
+ 
+            // mydb is database
+            // mydbuser is name of database
+            // mydbuser is password of database
+ 
+            Statement statement;
+            statement = connection.createStatement();
+            ResultSet resultSet;
+            resultSet = statement.executeQuery(
+                "select * from designation");
+            int code;
+            String title;
+            while (resultSet.next()) {
+                code = resultSet.getInt("code");
+                title = resultSet.getString("title").trim();
+                System.out.println("Code : " + code
+                                   + " Title : " + title);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        }
+        catch (Exception exception) {
+            System.out.println(exception);
+        }
+    } // function ends
+}
